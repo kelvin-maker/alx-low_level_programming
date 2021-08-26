cc02598827c0f72e2571fa7575c67186de0b49b0
kelvin-maker <dennismugaog97@gmail.com>
2021-08-26 01:29:53 -0700

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,26 +1,4 @@
 #include <stdio.h>
-/**
- * main - outputs to string
- *
- * Description: Prints out "and that piece of art is useful..." without printf
- *
- * Return: 1
- */
-int main(void)
-{
-  char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  long l = 59;
-  long fd = 1;
-  long syscall = 1;
-  long ret = 0;
-  __asm__ ("syscall"
-	   : "=a" (ret)
-	   : "a" (syscall),
-	     "D" (fd),
-	     "S" (s),
-	     "d" (l));
-  return (1);
-}#include <stdio.h>
  /**
   * main - outputs to string
   *
@@ -30,16 +8,16 @@ int main(void)
   */
 int main(void)
 {
-  char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  long l = 59;
-  long fd = 1;
-  long syscall = 1;
-  long ret = 0;
-  __asm__ ("syscall"
-	   : "=a" (ret)
-	   : "a" (syscall),
-	     "D" (fd),
-	     "S" (s),
-	     "d" (l));
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-19\n"
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
   return (1);
 }
