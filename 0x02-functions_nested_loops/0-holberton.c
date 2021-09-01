22997dde23b6d3e82abfe0245fe37d134e272328
kelvin-maker <dennismugaog97@gmail.com>
2021-09-01 04:00:53 -0700

    putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * main - prints holberton with __putchar()
+ * main - prints putchar with __putchar()
  *
  * Return: 0.
  */
@@ -9,7 +9,7 @@ int main(void)
 char str[] = "_putchar";
 int i = 0;
 
-while (i < 9)
+while (i < 8)
 {
 _putchar(str[i]);
 i++;
