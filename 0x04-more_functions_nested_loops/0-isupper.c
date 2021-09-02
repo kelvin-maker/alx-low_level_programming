06e0b4b88194571c1c41b030d2fb9d24392395b7
kelvin-maker <dennismugaog97@gmail.com>
2021-09-02 04:35:32 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <studio.h>
+/**
+ *
+ *main - check if upperrcase
+ *
+ *return: Always 0.
+ */
+int main() {
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+}
