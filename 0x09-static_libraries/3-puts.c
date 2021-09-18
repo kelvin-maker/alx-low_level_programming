d5f824944b3b0e97c1a123207cc30bc6d06b58bc
kelvin-maker <dennismugaog97@gmail.com>
2021-09-18 13:32:16 -0700

    static liblary

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: Print a string using followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
