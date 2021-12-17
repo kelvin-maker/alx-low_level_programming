b4d9d04bc1b8c3c81775a883970f6ce61b674894
kelvin-maker <dennismugaog97@gmail.com>
2021-12-17 07:31:06 -0800

    implimented 1-djb2.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,11 +6,10 @@
  * @size: the desired size of the new hash table
  *
  * Return: a pointer to the newly created hash table
- */
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *ht = malloc(sizeof(*ht));
-
 if (ht)
 {
 ht->array = calloc(size, sizeof(*ht->array));
