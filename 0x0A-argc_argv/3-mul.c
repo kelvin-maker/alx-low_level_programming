83677b234e2a33ecfa85b3e67f42c8da7dffa22c
Kelvin Mwenda <nkmke1964@gmail.com>
2022-07-08 12:57:00 -0700

    argv count

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,16 +7,16 @@
  *
  * Return: always 0
  */
+
+
 int main(int argc, char *argv[]  __attribute__((unused)))
 {
 int multiple;
-  
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
- 
 multiple = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", multiple);
 return (0);
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,21 +8,35 @@
  *
  * Return: always 0
  */
+
+
 int main(int argc, char *argv[]  __attribute__((unused)))
 {
-  int i = 0, sum = 0;
-  if (argc)
-    printf("%d\n", 0);
-  if (isdigit(atol(argv[i])) == 0)
-    {
-    printf("%s\n", "error");
-  return (1);
-    }
-    while(i < argc)
-      {
-	sum += atoi(argv[i]);
-	printf("%d\n", sum);
-	i++ ;
-      }
-  return (0);
+int i = 0; 
+int sum = 0;
+int j = 0;
+if (argc == 1)
+{
+printf("%s \n", "0");
+
+}
+for(j=1;j<argc;j++)
+{
+if (!isdigit(*argv[j]))
+{
+printf("%s \n", "error");
+exit(0);
+}
+}
+while(i < argc)
+{
+int arguement = atoi(argv[i]);
+sum = sum + arguement;
+if (isdigit(*argv[i]) && i == argc - 1)
+{
+printf("%d \n", sum);
+}
+i++;
+}
+return (0);
 }
